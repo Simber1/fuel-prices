e907c49ddd996209f7607be1cb904911d3fac8cf
Peter Stather <github@stather.me>
2024-01-04 20:12:53 +0000

    Add logging

--- fuel_prices/app.py
+++ fuel_prices/app.py
@@ -28,6 +28,7 @@ def get_data() -> dict:
     headers = {"user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/120.0.0.0 Safari/537.36'}
     for name, link in links.items():
+        print(f"Parsing {name}")
         parsed_data[name] = json.loads(requests.get(link, headers=headers).text)
     return parsed_data
 
